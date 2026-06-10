604da76e4e78d37d1e201d743c7ee0cc7b6ef98b
RomainSouveton <romain.souveton@iut-dijon.u-bourgogne.fr>
2026-06-10 09:37:39 +0200

    implémentation des annotations de l'ihm

--- Metier/Model/Grille.cs
+++ Metier/Model/Grille.cs
@@ -116,50 +116,12 @@ public class Grille
             throw new EGrilleValeurNulle("Aucune valeur sélectionnée");
 
         Case currentCase = cases[curseur.Ligne, curseur.Colonne];
-        if (!currentCase.Initiale && currentCase.Valeur == 0
-            && ValeurValide(curseur.Ligne, curseur.Colonne, valeurSelectionne.Value))
+        if (!currentCase.Initiale && !currentCase.Affiche
+            && currentCase.Valeur == valeurSelectionne.Value)
         {
-            currentCase.SetValeur(valeurSelectionne.Value);
+            currentCase.Reveler();
             currentCase.Choix.Clear();
         }
-    }
-
-    /// <summary>
-    /// Vérifie qu'une valeur peut être placée sans conflit (ligne, colonne, sous-grille)
-    /// </summary>
-    public bool ValeurValide(int ligne, int colonne, int valeur)
-    {
-        if (cases == null)
-            throw new EGrilleCharge("La grille n'est pas chargée");
-
-        // Vérifier la ligne
-        for (int c = 0; c < taille; c++)
-        {
-            if (c != colonne && cases[ligne, c].Affiche && cases[ligne, c].Valeur == valeur)
-                return false;
-        }
-
-        // Vérifier la colonne
-        for (int l = 0; l < taille; l++)
-        {
-            if (l != ligne && cases[l, colonne].Affiche && cases[l, colonne].Valeur == valeur)
-                return false;
-        }
-
-        // Vérifier la sous-grille
-        int racine = (int)Math.Sqrt(taille);
-        int sousLigneDebut = (ligne / racine) * racine;
-        int sousColonneDebut = (colonne / racine) * racine;
-        for (int l = sousLigneDebut; l < sousLigneDebut + racine; l++)
-        {
-            for (int c = sousColonneDebut; c < sousColonneDebut + racine; c++)
-            {
-                if ((l != ligne || c != colonne) && cases[l, c].Affiche && cases[l, c].Valeur == valeur)
-                    return false;
-            }
-        }
-
-        return true;
     }
 
     public void ChangerMode()
